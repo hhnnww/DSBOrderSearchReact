f97f17aea9b22cfa5f19097babc200b6c3ca8f0e
wuweihua <175990271@qq.com>
2024-12-16 18:41:23 +0800

    11111

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,13 @@
 import {
-  isRouteErrorResponse,
   Links,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from "react-router";
 
-import { ThemeProvider } from "@mui/material";
 
-import CssBaseline from "@mui/material/CssBaseline";
 import { client } from '../src/client';
-import type { Route } from "./+types/root";
-import { darkTheme } from "./theme";
 
 client.setConfig({
   baseURL: 'https://yumimi.top/api/',
@@ -39,37 +34,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
-    <Outlet />
-  </ThemeProvider>;
+  return <Outlet />
 }
 
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
 
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
-    details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
-  }
-
-  return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
-  );
-}
