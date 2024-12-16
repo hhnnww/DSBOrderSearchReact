bfdfa1b460d0ec5ba87acb015daea88b24a95790
wuweihua <175990271@qq.com>
2024-12-16 18:42:45 +0800

    12312

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,9 @@ import {
 } from "react-router";
 
 
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { client } from '../src/client';
+import { darkTheme } from "./theme";
 
 client.setConfig({
   baseURL: 'https://yumimi.top/api/',
@@ -25,7 +27,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <ThemeProvider theme={darkTheme} defaultMode="dark">
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
